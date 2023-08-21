fc47875be0c332074a9f36375c554ad01a6185de
mrkoc <mr.kocta@bk.ru>
2023-08-21 11:17:02 +0300

    feat(model user): Add a function to check the jwt token

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,7 +1,39 @@
 from pydantic import BaseModel, constr, EmailStr
+import jwt
+from app.core.security import SECRET_KEY, ALGORITHM
+from app.db.database import CheckUserInDatabase, SessionLocal
+from fastapi import HTTPException, status
+
+check_user = CheckUserInDatabase()
 
 
-class User(BaseModel):
+class UserModel(BaseModel):
     username: constr(min_length=3, max_length=12)
     password: int
     email: EmailStr
+
+    def verify_token(self, token):
+        if not token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен отсутствует")
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+            username = payload.get("sub")
+            user = check_user.get_user_by_name(SessionLocal, username)
+
+            if user:
+                return user
+            else:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                    detail="Пользователь не найден, проверьте правильность ввведенных данных")
+
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен истек")
+
+        except jwt.DecodeError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="токен не действителен")
+
+
+
+
+
