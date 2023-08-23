be2258096851c2f1138b40a4067b3b983a13297f
mrkoc <mr.kocta@bk.ru>
2023-08-23 12:05:04 +0300

    fix(user.model): Remove unnecessary fields, leave the fields that are required for registration and authentication

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,40 +1,13 @@
-from typing import List
-
+from typing import List, Union
 from pydantic import BaseModel, constr, EmailStr
-import jwt
-from app.core.security import SECRET_KEY, ALGORITHM
-from app.db.database import CheckUserInDatabase, SessionLocal
-from fastapi import HTTPException, status
+from app.db.database import CheckUserInDatabase, SessionLocal, User
 from .task import Task
-
-check_user = CheckUserInDatabase()
 
 
 class UserModel(BaseModel):
     username: constr(min_length=3, max_length=12)
     email: EmailStr
     tasks: List[Task]
-
-    def verify_token(self, token):
-        if not token:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен отсутствует")
-
-        try:
-            payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
-            username = payload.get("sub")
-            user = check_user.get_user_by_name(SessionLocal, username)
-
-            if user:
-                return user
-            else:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                    detail="Пользователь не найден, проверьте правильность ввведенных данных")
-
-        except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен истек")
-
-        except jwt.DecodeError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="токен не действителен")
 
 
 class UserCreateModel(BaseModel):
@@ -47,6 +20,15 @@ class UserResponseModel(BaseModel):
     id: int
     username: str
     email: str
+    password: str
+    token: str
 
 
+class UserLoginModel(BaseModel):
+    email: EmailStr
+    password: str
 
+
+class TokenData(BaseModel):
+    username: str | None = None
+    password: str | None = None
