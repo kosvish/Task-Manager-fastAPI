11457274505a130c657c69e2778c20590f6b404a
mrkoc <mr.kocta@bk.ru>
2023-08-23 12:02:55 +0300

    feat(routs): add new register rout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.endpoints.users import router
+from app.api.endpoints.users import register_router, login_router
 
 app = FastAPI()
 
-app.include_router(router, prefix="/register")
+app.include_router(register_router, prefix="/register")
+app.include_router(login_router, prefix="/login")
