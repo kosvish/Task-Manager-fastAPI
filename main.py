3ec1de7d06e4b2117de2859b6859adbd8151efba
mrkoc <mr.kocta@bk.ru>
2023-08-25 11:50:06 +0300

    feat(add): include new route, and fix olds

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.api.endpoints.users import register_router, login_router
+from app.api.endpoints.tasks import add_task_router
 
 app = FastAPI()
 
-app.include_router(register_router, prefix="/register")
-app.include_router(login_router, prefix="/login")
+app.include_router(register_router)
+app.include_router(login_router)
+app.include_router(add_task_router)
