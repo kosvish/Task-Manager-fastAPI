1b556aca0d94cf7645180458537201ec4e3ded31
mrkoc <mr.kocta@bk.ru>
2023-08-26 14:10:08 +0300

    feat(add): add CRUD routes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.api.endpoints.users import register_router, login_router
-from app.api.endpoints.tasks import add_task_router
+from app.api.endpoints.tasks import add_task_router, get_task_router, delete_task_router, put_task_router
 
 app = FastAPI()
 
 app.include_router(register_router)
 app.include_router(login_router)
 app.include_router(add_task_router)
+app.include_router(get_task_router)
+app.include_router(delete_task_router)
+app.include_router(put_task_router)
