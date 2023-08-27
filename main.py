24877f5983c434538f7824b1fca5503edf06a20c
mrkoc <mr.kocta@bk.ru>
2023-08-27 13:26:27 +0300

    feat(routes): add new html route

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.endpoints.users import register_router, login_router
-from app.api.endpoints.tasks import add_task_router, get_task_router, delete_task_router, put_task_router
+from app.api.endpoints.tasks import add_task_router, get_task_router, delete_task_router, put_task_router, html_router
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ app.include_router(add_task_router)
 app.include_router(get_task_router)
 app.include_router(delete_task_router)
 app.include_router(put_task_router)
+app.include_router(html_router)
