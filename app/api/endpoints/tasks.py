ca33d9071313d522fe2e97213cde2e1cb0b877af
mrkoc <mr.kocta@bk.ru>
2023-08-27 13:30:57 +0300

    feat(task.endpoint): add new logic for add_post, delete, and get_post

--- app/api/endpoints/tasks.py
+++ app/api/endpoints/tasks.py
@@ -1,5 +1,5 @@
-from app.api.models.task import TaskCreateModel, TaskResponseModel, TaskUpdateModel
-from fastapi import APIRouter, Depends, HTTPException, status
+from app.api.models.task import TaskCreateModel, TaskResponseModel
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 from app.db.database import get_db, Task, User
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
@@ -17,26 +17,19 @@ delete_task_router = APIRouter()
 put_task_router = APIRouter()
 html_router = APIRouter()
 
-def check_user(username: str, db: Session = Depends(get_db)):
-    check_username = db.query(User).filter(User.username == username).first()
+templates = Jinja2Templates(directory=templates_directory)
+
+
+@add_task_router.post("/add_task", response_model=TaskResponseModel)
+async def add_task(task: TaskCreateModel, db: Session = Depends(get_db),
+                   current_user: UserResponseModel = Depends(get_current_user)):
+    check_username = db.query(User).filter(User.username == current_user[1]).first()
     if not check_username:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Пользователя с данным именем не существует")
 
-    return True
-    pass
-
-
-@add_task_router.post("/{username}/add_task", response_model=TaskResponseModel)
-async def add_task(task: TaskCreateModel, username: str, db: Session = Depends(get_db)):
-    check_username = db.query(User).filter(User.username == username).first()
-    if not check_username:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Пользователя с данным именем не существует")
-
-    user_id = check_username.id
     task_data = task.dict()
-    task_data["user_id"] = user_id
+    task_data["user_id"] = current_user[0]
     new_task = Task(**task_data)
     db.add(new_task)
     db.commit()
@@ -44,39 +37,47 @@ async def add_task(task: TaskCreateModel, username: str, db: Session = Depends(g
     return new_task
 
 
-@get_task_router.get("/{username}/get_task")
-async def get_task(username: str, db: Session = Depends(get_db)):
-    check_user_db = db.query(User).filter(User.username == username).first()
+@get_task_router.get("/get_task")
+async def get_task(db: Session = Depends(get_db), current_user: UserResponseModel = Depends(get_current_user)):
+    check_user = db.query(User).filter(User.username == current_user[1]).first()
     if not check_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Пользователя с данным именем не существует")
-    user_id = check_user_db.id
+    user_id = current_user[0]
 
     tasks = db.query(Task).filter(Task.user_id == user_id).all()
 
     return tasks
 
 
-@delete_task_router.delete("/{username}/delete_task/{task_id}")
-async def delete_task(username: str, task_id: int, db: Session = Depends(get_db)):
-    check_user_db = db.query(User).filter(User.username == username).first()
+@delete_task_router.delete("/delete_task/{task_id}")
+async def delete_task(task_id: int, db: Session = Depends(get_db),
+                      current_user: UserResponseModel = Depends(get_current_user)):
+
+    check_user_db = db.query(User).filter(User.username == current_user[1]).first()
     if not check_user_db:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Пользователя с данным именем не существует")
 
     task = db.query(Task).filter(Task.id == task_id).first()
 
-    if not task:
+    if not task and task.user_id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задачи с таким id не существует")
+
+    if task.user_id != current_user[0]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Вы не можете удалять чужие задачи")
 
     db.delete(task)
     db.commit()
     return HTTPException(status_code=status.HTTP_202_ACCEPTED, detail="Задача успешно удалена")
 
 
-@put_task_router.put("/{username}/update_task/{task_id}", response_model=TaskResponseModel)
-async def put_task(username: str, task_id: int, completed: bool, db: Session = Depends(get_db)):
-    check_user_db = db.query(User).filter(User.username == username).first()
+@put_task_router.put("/update_task/{task_id}", response_model=TaskResponseModel)
+async def put_task(task_id: int, completed: bool,
+                   db: Session = Depends(get_db), current_user: UserResponseModel = Depends(get_current_user)):
+
+    check_user_db = db.query(User).filter(User.username == current_user[1]).first()
+
     if not check_user_db:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Пользователя с данным именем не существует")
@@ -86,6 +87,11 @@ async def put_task(username: str, task_id: int, completed: bool, db: Session = D
     if not task:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Задачи с таким id не существует")
+
+    if task.user_id != current_user[0]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Вы не можете редактировать чужие задачи"
+                            )
 
     task.completed = completed
 
