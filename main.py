3780da4e9e1178c975cf4ff73f4e0d9de4fe5c90
mrkoc <mr.kocta@bk.ru>
2023-08-28 10:39:12 +0300

    feat(main): add dashboard rout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.endpoints.users import register_router, login_router
-from app.api.endpoints.tasks import add_task_router, get_task_router, delete_task_router, put_task_router, html_router
+from app.api.endpoints.tasks import (add_task_router, get_task_router, delete_task_router, put_task_router,
+                                     html_router_personal_task, dashboard_router)
 
 app = FastAPI()
 
@@ -10,4 +11,5 @@ app.include_router(add_task_router)
 app.include_router(get_task_router)
 app.include_router(delete_task_router)
 app.include_router(put_task_router)
-app.include_router(html_router)
+app.include_router(html_router_personal_task)
+app.include_router(dashboard_router)
